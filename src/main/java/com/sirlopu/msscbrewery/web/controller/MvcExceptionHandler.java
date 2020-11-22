6e25e8b07c10cf157e0457cc03c401ff384c3034
Gabriel S <sirlopug@gmail.com>
2020-11-21 23:27:51 -0800

    Added BindException to the controller exception handler

--- src/main/java/com/sirlopu/msscbrewery/web/controller/MvcExceptionHandler.java
+++ src/main/java/com/sirlopu/msscbrewery/web/controller/MvcExceptionHandler.java
@@ -2,6 +2,7 @@ package com.sirlopu.msscbrewery.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,5 +19,11 @@ public class MvcExceptionHandler {
         e.getConstraintViolations().forEach(constraintViolation -> errors.add(constraintViolation.getPropertyPath() + " : " + constraintViolation.getMessage()));
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException ex){
+
+        return new ResponseEntity(ex.getAllErrors(), HttpStatus.BAD_REQUEST);
     }
 }
